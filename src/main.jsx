9b054dfc808981ff675e505367495fda846900bc
Panagiotis <panosxologkitas@gmail.com>
2026-06-14 19:02:59 +0300

    Revert experimental aesthetic changes

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
-import '@fontsource/geist-sans';
-import '@fontsource/geist-mono';
 import '../style.css'; // Make sure the path to css is correct
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
