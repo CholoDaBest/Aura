818890fff725d50db358e2c5446e3e5ea0c21b73
Panagiotis <panosxologkitas@gmail.com>
2026-06-14 18:53:50 +0300

    Fix fontsource packages

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
-import 'geist/font/sans';
-import 'geist/font/mono';
+import '@fontsource/geist-sans';
+import '@fontsource/geist-mono';
 import '../style.css'; // Make sure the path to css is correct
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
